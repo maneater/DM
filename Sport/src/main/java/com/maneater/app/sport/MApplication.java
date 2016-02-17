4de7c230014601b6bb33a0ee5262edcffffb0635
maneater <shanghliang@163.com>
2016-02-17 23:57:53 +0800

    update

---
+++ Sport/src/main/java/com/maneater/app/sport/MApplication.java
@@ -0,0 +1,11 @@
+package com.maneater.app.sport;
+
+import android.app.Application;
+
+public class MApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
