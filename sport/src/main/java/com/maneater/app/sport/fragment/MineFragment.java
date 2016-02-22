833acd10ed487c975e5f76473da17b9717a157ab
maneater <shanghliang@163.com>
2016-02-23 01:01:21 +0800

    update

--- sport/src/main/java/com/maneater/app/sport/fragment/MineFragment.java
+++ sport/src/main/java/com/maneater/app/sport/fragment/MineFragment.java
@@ -3,17 +3,24 @@ package com.maneater.app.sport.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.View;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import com.maneater.app.sport.R;
 import com.maneater.app.sport.account.XAccount;
 import com.maneater.app.sport.account.XAccountManager;
 import com.maneater.app.sport.net.Result;
+import com.maneater.app.sport.view.CurveView;
 import com.maneater.base.util.InjectUtil;
 import com.maneater.base.util.XImageLoader;
 import rx.subjects.PublishSubject;
 
-public class MineFragment extends BaseFragment {
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MineFragment extends BaseFragment implements SwipeRefreshLayout.OnRefreshListener {
 
 
     private PublishSubject<Result> loadUserDateCMD = PublishSubject.create();
@@ -21,7 +28,10 @@ public class MineFragment extends BaseFragment {
     public MineFragment() {
     }
 
-    private ImageView vImgHeaderView = null;
+    private ImageView vImgHeaderViewACK = null;
+    private CurveView vCurveView = null;
+    private SwipeRefreshLayout lytSwipeRefresh = null;
+    private HorizontalScrollView vLytHorizontal = null;
 
 
     @Override
@@ -48,11 +58,27 @@ public class MineFragment extends BaseFragment {
     protected void initData() {
         XAccount xAccount = XAccountManager.getInstance(getActivity()).getLoginAccount();
         //http://img4.duitang.com/uploads/item/201411/01/20141101172619_5sz2Y.jpeg
-        XImageLoader.getDefault(getContext()).displayImage("http://img4.duitang.com/uploads/item/201411/01/20141101172619_5sz2Y.jpeg", vImgHeaderView);
+        XImageLoader.getDefault(getContext()).displayImage("http://img4.duitang.com/uploads/item/201411/01/20141101172619_5sz2Y.jpeg", vImgHeaderViewACK);
+        vCurveView.render(randomData(), true);
+    }
+
+    private List<CurveView.CurveItem> randomData() {
+        List<CurveView.CurveItem> dataList = new ArrayList<CurveView.CurveItem>();
+        for (int i = 0; i < 31; i++) {
+            dataList.add(new CurveView.CurveItem(i + "", ((int) (Math.random() * 100))));
+        }
+        return dataList;
     }
 
     @Override
     protected void initListener() {
+        lytSwipeRefresh.setOnRefreshListener(this);
+    }
 
+    @Override
+    public void onRefresh() {
+        vLytHorizontal.scrollTo(0, 0);
+        initData();
+        lytSwipeRefresh.setRefreshing(false);
     }
 }
--- sport/src/main/java/com/maneater/app/sport/view/CurveView.java
+++ sport/src/main/java/com/maneater/app/sport/view/CurveView.java
@@ -1,8 +1,5 @@
 package com.maneater.app.sport.view;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.animation.ValueAnimator;
 import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.content.Context;
@@ -13,207 +10,245 @@ import android.graphics.Paint.Style;
 import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
-import android.view.GestureDetector.OnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Scroller;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CurveView extends View {
 
-	private int mCoverColor = 0x2e00FFB5;
-	private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-	private Path mPath = new Path();
-	private float mAnimateRadio = 0.0f;
+    private int mCoverColor = 0x2e00FFB5;
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Path mPath = new Path();
+    private float mAnimateRadio = 0.0f;
 
-	private int mLabelTextSize = 80;
-	// x最小间隔
-	private float minXInterval = 300;
+    private int mLabelTextSize = 50;
+    // x最小间隔
+    private int minXInterval = 300;
 
-	private float mP = 100;
-	private float mPaddingRight = 100;
-	private float mPaddingTop = 200;
-	private float mPaddingBottom = 200;
+    private float mPaddingLeft = 100;
+    private float mPaddingRight = 100;
+    private float mPaddingTop = 50;
+    private float mPaddingBottom = 50;
 
-	private int mDividerSize = 8;
-	private float mDividerLineWidth = 2;
-	private int mDividerColor = Color.GRAY;
+    private int mDividerSize = 8;
+    private float mDividerLineWidth = 2;
+    private int mDividerColor = Color.WHITE;
 
-	private float mLineWidth = 8;
-	private int mLineColor = 0xFF00FFB5;
+    private float mLineWidth = 8;
+    private int mLineColor = 0xFF00FFB5;
 
-	private int mCicleRadius = 18;
-	private int mCicleRadius2 = 12;
+    private int mCicleRadius = 18;
+    private int mCicleRadius2 = 12;
 
-	private GestureDetector mGestureDetector = null;
+    private GestureDetector mGestureDetector = null;
 
-	public CurveView(Context context) {
-		super(context);
-		init();
-	}
+    public CurveView(Context context) {
+        super(context);
+        init();
+    }
 
-	public CurveView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		init();
-	}
+    public CurveView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
 
-	public CurveView(Context context, AttributeSet attrs, int defStyleAttr) {
-		super(context, attrs, defStyleAttr);
-		init();
-	}
+    public CurveView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
 
-	private void init() {
-		mPaint.setTextSize(mLabelTextSize);
-		mGestureDetector = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener() {
-			@Override
-			public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-				scrollBy((int) distanceX, 0);
-				return true;
-			}
+    private void init() {
+        mPaint.setTextSize(mLabelTextSize);
+        mGestureDetector = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                int mRight = getRight();
+                int targetScrollX = (int) (getScrollX() + distanceX);
+                if (targetScrollX <= 0) {
+                    scrollTo(0, 0);
+                } else if (targetScrollX + mWindowWidth >= getWidth()) {
+                    scrollTo(getWidth() - mWindowWidth, 0);
+                } else {
+                    scrollBy((int) distanceX, 0);
+                }
+                return true;
+            }
 
-			@Override
-			public boolean onDown(MotionEvent e) {
-				return true;
-			}
-		});
-	}
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+        });
+    }
+//
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        return mGestureDetector.onTouchEvent(event);
+//    }
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		return mGestureDetector.onTouchEvent(event);
-	}
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+    }
 
-	@Override
-	protected void onScrollChanged(int l, int t, int oldl, int oldt) {
-		super.onScrollChanged(l, t, oldl, oldt);
-	}
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawDivider(canvas);
+        drawLine(canvas);
+    }
 
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-		drawDivider(canvas);
-		drawLine(canvas);
-	}
+    private void drawDivider(Canvas canvas) {
+        mPaint.setStyle(Style.STROKE);
+        mPaint.setStrokeWidth(mDividerLineWidth);
+        mPaint.setColor(mDividerColor);
+        float height = getMaxDrawHeight() / mDividerSize;
+        for (int i = 0; i <= mDividerSize + 1; i++) {
+            canvas.drawLine(mPaddingLeft, height * i + mPaddingTop, getWidth() - mPaddingRight, height * i + mPaddingTop, mPaint);
+        }
+    }
 
-	private void drawDivider(Canvas canvas) {
-		mPaint.setStyle(Style.STROKE);
-		mPaint.setStrokeWidth(mDividerLineWidth);
-		mPaint.setColor(mDividerColor);
-		float height = getHeight() / mDividerSize;
-		float width = getWidth();
-		for (int i = 1; i <= mDividerSize; i++) {
-			canvas.drawLine(0, height * i, width, height * i, mPaint);
-		}
-	}
 
-	private void drawLine(Canvas canvas) {
-		if (mMaxItem != null && dataItem != null && dataItem.size() > 0) {
+    //除去padding的高度
+    private float getMaxDrawHeight() {
+        return getHeight() - mPaddingBottom - mPaddingTop;
+    }
 
-			float maxHeight = getHeight() - mPaddingBottom - mPaddingTop;
+    //除去padding的宽度
+    private float getMaxDrawWidth() {
+        return getWidth() - mPaddingRight - mPaddingLeft;
+    }
 
-			float radioY = (float) (maxHeight / mMaxItem.value);
-			float xStep = (float) (getWidth() / dataItem.size());
-			int pointSize = dataItem.size();
+    private void drawLine(Canvas canvas) {
+        if (mMaxItem != null && dataItem != null && dataItem.size() > 0) {
 
-			float[] drawPoints = new float[pointSize * 2];
-			mPath.reset();
-			float mX = xStep / 2;
-			for (int i = 0; i < pointSize; i++) {
-				drawPoints[i * 2] = mX;
-				drawPoints[i * 2 + 1] = (float) (maxHeight - radioY * dataItem.get(i).value * mAnimateRadio);
+            float maxHeight = getHeight() - mPaddingTop - mPaddingBottom;
+            int pointSize = dataItem.size();
 
-				if (i == 0) {
-					mPath.moveTo(mX, maxHeight);
-				}
+            float radioY = (float) (maxHeight / mMaxItem.value);
+            float xStep = (getMaxDrawWidth() / pointSize);
 
-				mPath.lineTo(mX, drawPoints[i * 2 + 1]);
 
-				if (i == dataItem.size() - 1) {
-					mPath.lineTo(mX, maxHeight);
-					mPath.close();
-				}
-				mX += xStep;
-			}
+            float[] drawPoints = new float[pointSize * 2];
+            mPath.reset();
+            float mXPoint = mPaddingLeft;
 
-			mPaint.setStyle(Style.FILL_AND_STROKE);
-			mPaint.setColor(mCoverColor);
-			canvas.drawPath(mPath, mPaint);
+            if (xStep > minXInterval) {
+                mXPoint += (xStep / 2);
+            }
 
-			mPaint.setStyle(Style.STROKE);
-			mPaint.setStrokeWidth(mLineWidth);
-			mPaint.setColor(mLineColor);
-			canvas.drawPath(mPath, mPaint);
+            for (int i = 0; i < pointSize; i++) {
+                drawPoints[i * 2] = mXPoint;
+                drawPoints[i * 2 + 1] = (float) (getHeight() - mPaddingBottom - radioY * dataItem.get(i).value * mAnimateRadio);
 
-			// draw point and label
+                if (i == 0) {
+                    mPath.moveTo(mXPoint, getHeight() - mPaddingBottom);
+                }
 
-			mPaint.setStyle(Style.FILL);
-			for (int i = 0; i < pointSize; i++) {
-				float x = drawPoints[i * 2];
-				float y = drawPoints[i * 2 + 1];
-				CurveItem curveItem = dataItem.get(i);
-				mPaint.setColor(mLineColor);
-				canvas.drawCircle(x, y, mCicleRadius, mPaint);
-				mPaint.setColor(Color.WHITE);
-				canvas.drawCircle(x, y, mCicleRadius2, mPaint);
+                mPath.lineTo(mXPoint, drawPoints[i * 2 + 1]);
 
-				mPaint.setColor(mDividerColor);
-				float xOffset = -mPaint.measureText(String.valueOf(curveItem.value)) / 2;
+                if (i == dataItem.size() - 1) {
+                    mPath.lineTo(mXPoint, getHeight() - mPaddingBottom);
+                }
+                mXPoint += xStep;
+            }
 
-				float yOffset = (mPaint.ascent() - mPaint.descent()) / 2;
+            mPaint.setStyle(Style.FILL_AND_STROKE);
+            mPaint.setColor(mCoverColor);
+            canvas.drawPath(mPath, mPaint);
 
-				canvas.drawText(String.valueOf(curveItem.value), x + xOffset, y + yOffset, mPaint);
-			}
-		}
-	}
+//            mPath.close();
+            mPaint.setStyle(Style.STROKE);
+            mPaint.setStrokeWidth(mLineWidth);
+            mPaint.setColor(mLineColor);
+            canvas.drawPath(mPath, mPaint);
 
-	private CurveItem checkMaxItem(List<CurveItem> dataItem) {
-		CurveItem item = null;
-		for (CurveItem curveItem : dataItem) {
-			if (item == null || curveItem.value > item.value) {
-				item = curveItem;
-			}
-		}
-		return item;
-	}
+            // draw point and label
 
-	private List<CurveItem> dataItem = new ArrayList<CurveItem>();
-	private CurveItem mMaxItem = null;
+            mPaint.setStyle(Style.FILL);
+            for (int i = 0; i < pointSize; i++) {
+                float x = drawPoints[i * 2];
+                float y = drawPoints[i * 2 + 1];
+                CurveItem curveItem = dataItem.get(i);
+                mPaint.setColor(mLineColor);
+                canvas.drawCircle(x, y, mCicleRadius, mPaint);
+                mPaint.setColor(Color.WHITE);
+                canvas.drawCircle(x, y, mCicleRadius2, mPaint);
 
-	public void render(List<CurveItem> dataItem, boolean animate) {
-		this.dataItem = dataItem;
-		this.mMaxItem = checkMaxItem(dataItem);
-		if (animate && this.dataItem != null && this.dataItem.size() > 0) {
-			mAnimateRadio = 0.0f;
-			ValueAnimator valueAnimator = ValueAnimator.ofFloat(1);
-			valueAnimator.setDuration(1000);
-			valueAnimator.addUpdateListener(new AnimatorUpdateListener() {
+                mPaint.setColor(mDividerColor);
+                float xOffset = -mPaint.measureText(String.valueOf(curveItem.value)) / 2;
+                float yOffset = (mPaint.ascent() - mPaint.descent()) / 2;
+                canvas.drawText(String.valueOf(curveItem.value), x + xOffset, y + yOffset, mPaint);
+                xOffset = -mPaint.measureText(String.valueOf(curveItem.label)) / 2;
+                canvas.drawText(String.valueOf(curveItem.label), x + xOffset, getHeight(), mPaint);
+            }
+        }
+    }
 
-				@Override
-				public void onAnimationUpdate(ValueAnimator animation) {
-					mAnimateRadio = (Float) animation.getAnimatedValue();
-					invalidate();
-				}
-			});
-			valueAnimator.start();
-		} else {
-			invalidate();
-		}
-	}
+    private CurveItem checkMaxItem(List<CurveItem> dataItem) {
+        CurveItem item = null;
+        for (CurveItem curveItem : dataItem) {
+            if (item == null || curveItem.value > item.value) {
+                item = curveItem;
+            }
+        }
+        return item;
+    }
 
-	@Override
-	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-	}
+    private List<CurveItem> dataItem = new ArrayList<CurveItem>();
+    private CurveItem mMaxItem = null;
 
-	public static class CurveItem {
+    public void render(List<CurveItem> dataItem, boolean animate) {
+        this.dataItem = dataItem;
+        scrollTo(0, 0);
+        requestLayout();
+        this.mMaxItem = checkMaxItem(dataItem);
+        if (animate && this.dataItem != null && this.dataItem.size() > 0) {
+            mAnimateRadio = 0.0f;
+            ValueAnimator valueAnimator = ValueAnimator.ofFloat(1);
+            valueAnimator.setDuration(1000);
+            valueAnimator.addUpdateListener(new AnimatorUpdateListener() {
 
-		public CurveItem(String label, double value) {
-			this.label = label;
-			this.value = value;
-		}
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    mAnimateRadio = (Float) animation.getAnimatedValue();
+                    invalidate();
+                }
+            });
+            valueAnimator.start();
+        } else {
+            invalidate();
+        }
+    }
 
-		String label;
-		double value;
-		double tmpValue;
-	}
+    private int mWindowWidth = 0;
+
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int mWidth = getMeasuredWidth();
+        if (this.dataItem != null) {
+            //最小宽度
+            int needWidth = minXInterval * this.dataItem.size();
+            if (mWidth < needWidth) {
+                setMeasuredDimension(needWidth, getMeasuredHeight());
+            }
+        }
+    }
+
+    public static class CurveItem {
+
+        public CurveItem(String label, double value) {
+            this.label = label;
+            this.value = value;
+        }
+
+        String label;
+        double value;
+    }
 
 }
